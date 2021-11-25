af033ecdc17770bbb40cc35c41039855b988e6b9
gturner24 <91153703+gturner24@users.noreply.github.com>
2021-11-24 22:05:52 -0600

    Update dataFunctions.py

--- dataFunctions.py
+++ dataFunctions.py
@@ -1,7 +1,35 @@
 import pandas as pd
 
-def readItems(csv):
-    df = pd.read_csv(csv)
-    print(df)
+class menuItem:
 
-readItems('availableItems.csv')
+    def __init__(self, itemName, itemPrice, itemCategory):
+        self.itemName = itemName
+        self.itemPrice = itemPrice
+        self.itemCategory = itemCategory
+
+    def setItemName(self, name):
+        self.itemName = name
+
+    def setItemPrice(self, price):
+        self.itemPrice = price
+
+    def setItemCategory(self, category):
+        self.itemCategory = category
+
+
+    def getItemName(self):
+        return self.itemName
+
+    def getItemPrice(self):
+        return self.itemPrice
+
+    def getItemCategory(self):
+        return self.itemCategory
+
+    def __str__(self):
+        itemString = self.itemName + "\n"
+        itemString += "Price: " + self.itemPrice + "\n"
+        itemString += "Category: " + self.itemCategory + "\n"
+        return itemString
+
+
