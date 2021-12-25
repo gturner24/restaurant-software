f107ef61d021a09505f7dccabbc4259cca379368
gturner24 <91153703+gturner24@users.noreply.github.com>
2021-12-25 01:00:23 -0500

    Add files via upload

--- mainClasses.py
+++ mainClasses.py
@@ -78,47 +78,100 @@ class menuFrame(ttk.Frame):
         tableNumber = 12
         tableList = []
         for table in range(0, tableNumber):
-            tableList.append(f'Table {table + 1}')
+            tableList.append(table + 1)
 
         self.availableItems = restaurantInfo.readRestaurantInfo('availableItems.csv')
         self.itemList = self.availableItems[0]
         self.priceList = self.availableItems[1]
         self.categoryList = self.availableItems[2]
 
+        tableLabel = ttk.Label(self, text='Table Number: ', font=('Segoe UI', 16))
+        tableLabel.grid(row=0, column=0)
+
         self.selectedTable = tk.StringVar()
         tableSelector = ttk.Combobox(self, textvariable=self.selectedTable, width=50, font=('Segoe UI', 16))
         tableSelector['values'] = tableList
         tableSelector['state'] = 'readonly'
-        tableSelector.set('Set Table Number...')
-        tableSelector.grid(row=0, column=0, columnspan=3, padx=15, pady=15)
+        tableSelector.current(0)
+        tableSelector.bind('<<ComboboxSelected>>', self.showListbox)
+        tableSelector.grid(row=0, column=1, columnspan=2, padx=15, pady=15)
 
 
         COLUMNS = 3
 
         for i, item in enumerate(self.itemList):
-            lbl = ttk.Button(self, text=item, style='my.TButton', command=lambda price=self.priceList[i]: self.calculate(price))
+            lbl = ttk.Button(self, text=item, style='my.TButton', command=lambda item=self.itemList[i]: [self.updateReceipt(item), self.showListbox('')])
             row, column = divmod(i, COLUMNS)
             lbl.grid(row=row + 1, column=column, sticky='news', ipadx=25, ipady=25)
 
         self.tableInfoFrame = ttk.Frame(self)
-        self.tableInfoFrame.grid()
+        self.tableInfoFrame.grid(row=1, column=3, padx=15, pady=15, rowspan=5)
+
+        self.receiptItems = []
+        self.receiptPrices = []
+        self.receiptItemsDICT = {}
+        self.receiptPricesDICT = {}
+
+        for table in range(tableNumber):
+            self.receiptItemsDICT[table + 1] = []
+            self.receiptPricesDICT[table + 1] = []
+
+        self.selectedReceiptItem = tk.StringVar()
+        self.receiptBox = tk.Listbox(self.tableInfoFrame, listvariable=self.selectedReceiptItem)
+        self.receiptBox.pack()
 
         totalLabel = ttk.Label(self.tableInfoFrame, textvariable=self.tableTotal)
         totalLabel.pack()
-
-        resetTotalButton = ttk.Button(self.tableInfoFrame, text='Reset Total', command=self.resetTotal)
+        resetTotalButton = ttk.Button(self.tableInfoFrame, text='Reset Receipt', command=self.resetReceipt)
         resetTotalButton.pack()
 
-
-    def calculate(self, amount):
-        total = self.tableTotal.get()  # get current total
-        total += amount
-        self.tableTotal.set(round(total, 2))  # update total
+        deleteSelectedItemButton = ttk.Button(self.tableInfoFrame, text='Delete Selected Item', command=self.deleteSelectedItem)
+        deleteSelectedItemButton.pack()
 
 
-    def resetTotal(self):
-        total = 0
-        self.tableTotal.set(round(total, 2))
+
+    def updateReceipt(self, item):
+        self.receiptItemsDICT[int(self.selectedTable.get())].append(item)
+
+        self.receiptPricesDICT[int(self.selectedTable.get())].clear()  # this dictionary must be cleared because it iterates through another dictionary
+
+        for item in self.receiptItemsDICT[int(self.selectedTable.get())]:
+            if item in self.itemList:
+                self.receiptPricesDICT[int(self.selectedTable.get())].append(self.priceList[self.itemList.index(item)])
+
+
+    def resetReceipt(self):
+        self.receiptPricesDICT[int(self.selectedTable.get())].clear()  # resets the receipt prices for the specified table
+        self.receiptItemsDICT[int(self.selectedTable.get())].clear()   # resets the receipt items for the specified table
+        self.tableTotal.set(round(sum(self.receiptPricesDICT[int(self.selectedTable.get())]), 2))
+
+        self.receiptBox.delete(0, 'end')
+        for item in self.receiptItemsDICT[int(self.selectedTable.get())]:
+            self.receiptBox.insert(0, item)
+        self.tableTotal.set(round(sum(self.receiptPricesDICT[int(self.selectedTable.get())]), 2))
+
+    def deleteSelectedItem(self):
+        items = self.receiptBox.curselection()
+        for item in items:
+            menuItem = self.receiptBox.get(item)
+        self.receiptItemsDICT[int(self.selectedTable.get())].remove(menuItem)
+
+        for i, thing in enumerate(self.itemList):
+            if thing == menuItem:
+                price = self.priceList[i]
+                self.receiptPricesDICT[int(self.selectedTable.get())].remove(price)
+
+        self.receiptBox.delete(0, 'end')
+        for item in self.receiptItemsDICT[int(self.selectedTable.get())]:
+            self.receiptBox.insert(0, item)
+        self.tableTotal.set(round(sum(self.receiptPricesDICT[int(self.selectedTable.get())]), 2))
+
+
+    def showListbox(self, event):
+        self.receiptBox.delete(0, 'end')
+        for item in self.receiptItemsDICT[int(self.selectedTable.get())]:
+            self.receiptBox.insert(0, item)
+        self.tableTotal.set(sum(self.receiptPricesDICT[int(self.selectedTable.get())]))
 
 
 class employeeManagementFrame(ttk.Frame):
